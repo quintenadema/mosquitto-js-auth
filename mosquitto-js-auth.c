6852a73a08b78dc2644366689bcbd7717018b92f
Quinten Adema <quinten@ademagroup.com>
2024-08-24 15:05:49 +0200

    Update mosquitto-js-auth.c

--- mosquitto-js-auth.c
+++ mosquitto-js-auth.c
@@ -10,19 +10,30 @@ static int auth_callback(int event, void *event_data, void *user_data) {
 }
 
 // Plugin version function
-int mosquitto_auth_plugin_version(int supported_version_count, const int *supported_versions) {
+int mosquitto_auth_plugin_version(void) {
     return MOSQ_AUTH_PLUGIN_VERSION; // Return the plugin version
 }
 
 // Plugin initialization function
-int mosquitto_auth_plugin_init(mosquitto_plugin_id_t *plugin_id, void **user_data, struct mosquitto_auth_opt *options, int option_count) {
+int mosquitto_auth_plugin_init(void **user_data, struct mosquitto_opt *options, int option_count) {
     mosquitto_log_printf(MOSQ_LOG_INFO, "Mosquitto plugin initialized.");
-    mosquitto_callback_register(plugin_id, MOSQ_EVT_BASIC_AUTH, auth_callback, NULL, NULL); // Register the auth callback
+    
+    mosquitto_plugin_id_t *plugin_id = (mosquitto_plugin_id_t *)(*user_data);
+    
+    // Register the auth callback
+    mosquitto_callback_register(plugin_id, MOSQ_EVT_BASIC_AUTH, auth_callback, NULL, NULL);
+    
     return MOSQ_ERR_SUCCESS; // Initialization successful
 }
 
 // Plugin cleanup function
-int mosquitto_auth_plugin_cleanup(void *user_data, struct mosquitto_auth_opt *options, int option_count) {
+int mosquitto_auth_plugin_cleanup(void *user_data, struct mosquitto_opt *options, int option_count) {
     mosquitto_log_printf(MOSQ_LOG_INFO, "Mosquitto plugin cleanup.");
+    
+    mosquitto_plugin_id_t *plugin_id = (mosquitto_plugin_id_t *)user_data;
+    
+    // Unregister the auth callback
+    mosquitto_callback_unregister(plugin_id, MOSQ_EVT_BASIC_AUTH, auth_callback, NULL);
+    
     return MOSQ_ERR_SUCCESS; // Cleanup successful
 }
